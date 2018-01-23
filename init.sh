afcf9c7b12e711d1e31fe2a1a85c60f4af43c2a7
Edwin.JH.Lee <edwinjhlee@users.noreply.github.com>
2018-01-23 15:43:58 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,63 @@
+#! /usr/bin/env bash
+
+# source <(curl https://edwinjhlee.github.io/init.sh)
+
+# source <(curl https://edwinjhlee.github.io/el-logic/lib/chalk.sh)
+
+gen_lib_path(){
+    local local_lib_root=/tmp/libs
+    mkdir -p "$local_lib_root"
+    echo "$local_lib_root/$1"
+}
+
+include_lib_from_cache_or_web(){
+    local lib=${1:?"Please provide library name"}
+    local url=${2:?"Please provice url"}
+    local local_lib_path=$(gen_lib_path $lib)
+    
+    if [ ! -f $local_lib_path ]; then
+        curl $url >$local_lib_path
+    fi
+    source $local_lib_path
+    # source <([ -e $local_lib_path ] && cat $local_lib_path || curl $url >$local_lib_path)
+}
+
+include_ellib_from_cache_or_web(){
+    local lib=${1:?"Please provide library name"}
+    include_lib_from_cache_or_web $lib https://edwinjhlee.github.io/el-logic/lib/$lib
+}
+
+include_ellib_from_cache_or_web_all(){
+    for i in $*; do include_ellib_from_cache_or_web $i; done
+}
+
+MINIMUM=(
+    chalk.sh
+)
+
+STANDARD=(
+    ${MINIMUM[@]}
+    apps.sh
+)
+
+DY=(
+    ${STANDARD[@]}
+    dy.sh
+)
+
+DG=(
+    ${STANDARD[@]}
+    dg.sh
+)
+
+include_pack(){
+    case "$1" in
+        "min") include_ellib_from_cache_or_web_all ${MINIMUM[*]} ;;
+        "sta") include_ellib_from_cache_or_web_all ${STANDARD[*]} ;;
+        "dy" ) include_ellib_from_cache_or_web_all ${DY[*]} ;;
+        "dg" ) include_ellib_from_cache_or_web_all ${DG[*]} ;;
+        *) ;;
+    esac
+}
+
+include_pack $1
